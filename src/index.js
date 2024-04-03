99089836a09806d5e702948aee93614ceed1b2a2
Samir Taborda <stabordasierra@gmail.com>
2024-04-03 14:43:55 +1100

    React router v6 installed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { HeroesApp } from './HeroesApp';
 import './styles.css';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HeroesApp />
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
