3729cb0bf29cf30f38f2ab3a0a3708b0130de437
Samir Taborda <stabordasierra@gmail.com>
2024-04-06 22:27:01 +1100

    Login working

--- src/auth/LoginPage.jsx
+++ src/auth/LoginPage.jsx
@@ -1,19 +1,26 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 
 
 
 export const LoginPage = () => {
 
+    const { login } = useContext( AuthContext );
     const navigate = useNavigate();
+
     const onLogin = () => {
-        navigate('/marvel', {
+
+        login('Samir Taborda');
+
+        navigate('/', {
             replace: true
         })
     }
 
     return (
         <div className="container mt-5">
-            <h1>LoginPage</h1>
+            <h1>Login</h1>
             <hr />
 
             <button
