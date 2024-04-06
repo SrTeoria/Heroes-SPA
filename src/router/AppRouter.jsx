9f3291af19543b6d9b67a65972cdd43c43f5eb8b
Samir Taborda <stabordasierra@gmail.com>
2024-04-06 23:57:24 +1100

    Public routes working

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 
 export const AppRouter = () => {
@@ -10,7 +11,11 @@ export const AppRouter = () => {
         <>
             <Routes>
 
-                <Route path="login" element={<LoginPage />} />
+                <Route path='/login' element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                } />
 
                 <Route path="/*" element={
                     <PrivateRoute>
