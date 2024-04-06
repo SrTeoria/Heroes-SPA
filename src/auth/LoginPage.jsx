d673ddee42875d1414a62bb5b687c46637ec706b
Samir Taborda <stabordasierra@gmail.com>
2024-04-07 07:42:49 +1000

    Login page updated

--- src/auth/LoginPage.jsx
+++ src/auth/LoginPage.jsx
@@ -27,7 +27,7 @@ export const LoginPage = () => {
                 className="btn btn-primary"
                 onClick={ onLogin }
             >
-                Login
+                Login Page
             </button>
         </div>
     );
